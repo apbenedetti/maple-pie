fa13e32f55cea2c350f36b6ddf88bef6d70b7385
Anthony Benedetti <anthony.benedetti@gmail.com>
2025-01-30 19:36:53 -0500

    convert project to http server

--- cmd/maple-pie-server/main.go
+++ cmd/maple-pie-server/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/apbenedetti/maple-pie/internal/opencanada"
 )
 
 func main() {
 
-	fmt.Print(opencanada.ShowPackageMetadata("065439a9-c194-4259-9c95-245a852be4a1"))
-	fmt.Print(opencanada.SearchDataStoreResource("d9ff2144-4591-4ef5-b313-be5012eca3be"))
+	http.HandleFunc("/", handler)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, opencanada.ShowPackageMetadata("065439a9-c194-4259-9c95-245a852be4a1"))
+	fmt.Fprint(w, opencanada.SearchDataStoreResource("d9ff2144-4591-4ef5-b313-be5012eca3be"))
 }
