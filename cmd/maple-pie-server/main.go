d1fe5da422b08e0d5f007935b146d4d69dc81f4d
Anthony Benedetti <anthony.benedetti@gmail.com>
2025-05-17 20:29:49 -0400

    wip: messing about with json

--- cmd/maple-pie-server/main.go
+++ cmd/maple-pie-server/main.go
@@ -9,5 +9,5 @@ import (
 func main() {
 
 	fmt.Print(opencanada.ShowPackageMetadata("065439a9-c194-4259-9c95-245a852be4a1"))
-	fmt.Print(opencanada.SearchDataStoreResource("d9ff2144-4591-4ef5-b313-be5012eca3be"))
+	// fmt.Print(opencanada.SearchDataStoreResource("d9ff2144-4591-4ef5-b313-be5012eca3be"))
 }
